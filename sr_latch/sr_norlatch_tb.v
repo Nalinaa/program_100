71e96bf63aa97da555545a3d08df03a39de3220e
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-02-06 10:11:55 +0530

    Create sr_norlatch_tb.v

---
+++ sr_latch/sr_norlatch_tb.v
@@ -0,0 +1,24 @@
+module sr_norlatch_tb();
+  reg S,R;
+  wire Q,Qn;
+  sr_norlatch dut(S,R,Q,Qn);
+  initial
+    begin
+      $dumpfile("srl.vcd");
+      $dumpvars(1,sr_norlatch_tb);
+      end
+  
+initial 
+  begin
+    $monitor($time,"\tS=%b;R=%b;Q=%b;Qn=%b",S,R,Q,Qn);
+    S = 1;R = 1;
+    #10S = 0;
+    #10 S = 1;
+    #10 R = 0;
+    #10 R = 1;
+    #10 S = 0;R = 0;
+    #10 S = 1;R = 1;
+    #10 S = 0;R = 0;
+    #10$finish ;
+end
+endmodule
