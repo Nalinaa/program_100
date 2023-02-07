eac1c3f914fce04f0177af75686697a0a3ed481d
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-02-07 20:57:46 +0530

    Create  ripple_counter_tb.v

---
+++ ripple_counter/ripple_counter_tb.v
@@ -0,0 +1,18 @@
+module ripple_counter_tb;
+  reg t0,t1,t2,clk;
+  wire [2:0] out;
+  ripple_counter rc(t0,t1,t2,clk,out);
+  initial
+    begin
+      $dumpfile("rc.vcd");
+      $dumpvars(1,ripple_counter_tb);
+    end
+  initial
+    begin
+      clk=0;
+      $monitor($time,"\tout=%b",out);
+      
+    end
+  always #5 clk=~clk;
+endmodule
+      
