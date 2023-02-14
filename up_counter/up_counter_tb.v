ca4513ad351b14cabaf2d77fea24b51973128d19
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-02-14 12:53:27 +0530

    Create up_counter_tb.v

---
+++ up_counter/up_counter_tb.v
@@ -0,0 +1,21 @@
+module up_counter_tb();
+reg clk, reset;
+wire [3:0] counter;
+
+up_counter dut(clk, reset, counter);
+initial begin 
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+reset=1;
+#20;
+reset=0;
+end
+initial
+    begin
+      $dumpfile("dump.vcd"); 
+      $dumpvars(1);
+      #10000$finish;
+    end
+endmodule 
